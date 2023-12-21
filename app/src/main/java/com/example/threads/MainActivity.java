16076baeb1f00a43681d6ea578c0c52f2d713c23
kobay9iq <kby9iq@gmail.com>
2023-12-22 01:59:19 +0300

    1

--- app/src/main/java/com/example/threads/MainActivity.java
+++ app/src/main/java/com/example/threads/MainActivity.java
@@ -13,14 +13,13 @@ import com.example.threads.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
+  private static final Object lock = new Object();
 
   boolean notStarted = true;
-  private boolean switchTurn = true;
   String output = "";
   protected static Object[] turns = {new Object(), new Object(), new Object()};
+  protected static int nextTurn = 1;
   protected static Handler msgHandler;
-  protected static Handler turnHandler;
-  protected static int currentTurn = 1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -41,79 +40,51 @@ public class MainActivity extends AppCompatActivity {
       }
     };
 
-    turnHandler = new Handler(Looper.getMainLooper()) {
-      @Override
-      public void handleMessage(@NonNull Message msg) {
-        super.handleMessage(msg);
-        switchTurn = (boolean) msg.obj;
-      }
-    };
-
 
     EditText[] editTexts = {binding.ETTop, binding.ETCenter, binding.ETBottom};
 
-    binding.GOButton.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        int nextTurn = 0;
+    binding.GOButton.setOnClickListener(
+        new OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            if (notStarted) {
+              MyThread firstThread = new MyThread(editTexts[0].getText().toString());
+              MyThread secondThread = new MyThread(editTexts[1].getText().toString());
+              MyThread thirdThread = new MyThread(editTexts[2].getText().toString());
 
-        if (notStarted) {
-          MyThread firstThread = new MyThread(editTexts[0].getText().toString(), turns[0]);
-          MyThread secondThread = new MyThread(editTexts[1].getText().toString(), turns[1]);
-          MyThread thirdThread = new MyThread(editTexts[2].getText().toString(), turns[2]);
+              firstThread.start();
+              secondThread.start();
+              thirdThread.start();
 
+              try {
+                synchronized (turns[1]) {
+                  turns[1].wait();
+                }
+                synchronized (turns[2]) {
+                  turns[2].wait();
+                }
+              } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+              }
 
-          try {
-            synchronized (turns[0]) {
-              turns[0].wait();
-            }
-            synchronized (turns[1]) {
-              turns[1].wait();
-            }
-            synchronized (turns[2]) {
-              turns[2].wait();
-            }
-          } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-          }
-
-          firstThread.start();
-          secondThread.start();
-          thirdThread.start();
-
-
-
-          notStarted = false;
-          for (EditText et : editTexts) {
-            et.setFocusable(false);
-            et.setLongClickable(false);
-            et.setCursorVisible(false);
-          }
-
-
-          while (true) {
-            if (switchTurn) {
-              turns[nextTurn % 3].notify();
-              nextTurn++;
-              switchTurn = false;
+              notStarted = false;
+              for (EditText et : editTexts) {
+                et.setFocusable(false);
+                et.setLongClickable(false);
+                et.setCursorVisible(false);
+              }
             }
           }
-        }
-      }
-    });
-
-
+        });
   }
 }
 
 class MyThread extends Thread {
   private char[] TextToView;
   private String text;
-  private Object obj;
 
-  public MyThread(String text, Object obj) {
+  public MyThread(String text) {
     this.text = text;
-    this.obj = obj;
     TextToView = new char[text.length()];
   }
 
@@ -122,7 +93,7 @@ class MyThread extends Thread {
     super.run();
     char[] textChars = text.toCharArray();
 
-    synchronized (obj) {
+    synchronized (MainActivity.turns[MainActivity.nextTurn % 3 - 1]) {
       for (int i = 0; i != textChars.length; i++) {
         char ch = textChars[i];
         TextToView[i] = ch;
@@ -140,10 +111,9 @@ class MyThread extends Thread {
           }
 
           if (ch == ' ') {
-            Message turnOver = new Message();
-            turnOver.obj = true;
-            MainActivity.turnHandler.sendMessage(msg);
-            obj.wait();
+            MainActivity.turns[MainActivity.nextTurn % 3].notify();
+            MainActivity.turns[MainActivity.nextTurn % 3 - 1].wait();
+
           }
         } catch (InterruptedException e) {
           throw new RuntimeException(e);
